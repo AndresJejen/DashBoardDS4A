9e09c16bc1b2c8bf12941e5f73ba3c0eeb976030
AndresJejen <gajcam@gmail.com>
2020-05-23 17:52:39 -0500

    [Feat] DataBase Stay Safe

--- app.py
+++ app.py
@@ -2,17 +2,33 @@ import dash
 import dash_bootstrap_components as dbc
 from DAO.ElasticSearchQuery import ElasticSearchQuery
 from Models.ProjectModel import ProjectModel
+import os
+from pathlib import Path  # python3 only
+from dotenv import load_dotenv
 
+# OR, explicitly providing path to '.env'
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
+# Server configuration
 styles = [dbc.themes.SIMPLEX]
 app = dash.Dash(__name__, external_stylesheets=styles)
 server = app.server
 app.config.suppress_callback_exceptions = True
 
-user = 'Andres'
-password = 'Andres123*'
-url = "search-ds4a1-ewn5ngrlak4w5vxytn4h3h54za.us-east-1.es.amazonaws.com"
+# Connection Settings
+user = os.getenv('ES_USER')
+password = os.getenv('ES_PASS')
+url = os.getenv('ES_URL')
 es = ElasticSearchQuery(user, password, url=url)
 
+# Basic Data for common pages
 basic_data = [es.gq_max_min_timestamp(), es.gq_all_events_dataframe()]
 
+# Random Forest Model
 modelo = ProjectModel()
+
+
+
+
+
