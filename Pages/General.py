cc4b7daa0b59427609312ebb28e490879b11a5f0
AndresJejen <gajcam@gmail.com>
2020-06-01 18:41:41 -0500

    [Fix] Titles on persons conversion chart

--- Pages/General.py
+++ Pages/General.py
@@ -59,7 +59,7 @@ def changerange_csv(year):
     datos_condition_conversion = es.gq_count_condition_purchases(since, to)
 
     prices_conversion = Histogram(datos_prices_conversion['price'], "Number of conversions by groups of Prices by Day", "Price", "# of conversions")
-    person_conversion = Histogram(datos_person_conversion['Total Purchases'], "Number of conversions by Persons by date", "Person", "# of conversions")
+    person_conversion = Histogram(datos_person_conversion['Total Purchases'], "Number of persons by number ob conversions by date", "# of conversions", "# of persons")
     brand_conversion = barplot(datos_brand_conversion['Brand'], datos_brand_conversion['Total'], "Number of conversions by Brand in range of time", "# of conversions", "Brands")
     condition_conversion = barplot(datos_condition_conversion['Brand'], datos_condition_conversion['Total'], "Number of conversions by Quality status of device in range of time", "# of conversions", "Quality status")
 
